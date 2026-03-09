1ebd313b2e5796012309b672022797574899f2e0
siddharth <siddharthchandrakar007@gmail.com>
2026-03-09 15:57:08 +0530

    fix

---
+++ config/database.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"core/models"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+)
+
+var db *gorm.DB
+var err error
+
+func DbInit() {
+	config := GetConfig()
+	connectString := fmt.Sprintf(config.Dburl)
+
+	// Open the connection to the database
+	db, err = gorm.Open(postgres.Open(connectString), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Warn),
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+
+	if err != nil {
+		log.Fatalf("DB Connection Error: %v", err)
+	}
+
+	fmt.Println("Connected to Database")
+
+	// Uncomment if you have models to migrate
+	err = db.AutoMigrate(&models.CommitFileEmbedding{})
+	if err != nil {
+		log.Fatalf("Migration failed: %v", err)
+	}
+	fmt.Println(config.DbHost, config.DbName)
+}
+
+func DbManager() *gorm.DB {
+	return db
+}
