10e0adad7bb0bebebb8a4a8bcb40e5766ad2352c
siddharth <siddharthchandrakar007@gmail.com>
2026-03-24 09:52:25 +0530

    fix:cors issue fix

--- route/router.go
+++ route/router.go
@@ -11,7 +11,7 @@ func InitHttp() *echo.Echo {
 	app := App()
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"https://book-finder0908sid.netlify.app", "http://localhost:3000"}, // Add your frontend URLs
+		AllowOrigins:     []string{"https://book-finder0908sid.netlify.app", "http://localhost:3000", "https://commitlens.tech"}, // Add your frontend URLs
 		AllowMethods:     []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 		AllowCredentials: true,
